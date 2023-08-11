fd98d9d0f568e84bacb1b1675aa4cfe103375e73
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-08-12 00:13:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='aap'>
+        <img src='https://images.pexels.com/photos/14894269/pexels-photo-14894269.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'  />
+
+        Mountain View was integral to the early history and growth of Silicon Valley, and is the location of many high technology companies.
+         In 1956, William Shockley established Shockley Semiconductor Laboratory in Mountain View, the first company to develop 
+         silicon semiconductor devices in Silicon Valley. Today, Mountain View houses the headquarters of many of the world's largest 
+         technology companies, including Google and Alphabet Inc., Unicode Consortium, Intuit, NASA Ames Research Center, and major
+          former and current headquarter offices for Microsoft, Symantec, 23andMe, LinkedIn, Samsung, and Synopsys.
+      </div>
+      <div className='aap'>
+          <img src='https://images.pexels.com/photos/15471913/pexels-photo-15471913/free-photo-of-green-sea-wave-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'  />
+          A forest is an area of land dominated by trees.[1] Hundreds of definitions of forest are used throughout the world, incorporating factors such as tree density,
+           tree height, land use, legal standing, and ecological function.[2][3][4] The United Nations' Food and Agriculture Organization (FAO)
+            defines a forest as, "Land spanning more than 0.5 hectares with trees higher than 5 meters and a canopy cover of more than 10 percent, 
+            or trees able to reach these thresholds in situ. It does not include land that is predominantly under agricultural or urban use."[5]
+             Using this definition, Global Forest Resources Assessment 2020 (FRA 2020) found that forests covered 4.06 billion hectares 
+             (10.0 billion acres; 40.6 million square kilometres; 15.7 million square miles), or approximately 31 percent of the world's 
+             land area in 2020
+      </div>
+      <div className='aap'>
+        <img src='https://images.pexels.com/photos/6463841/pexels-photo-6463841.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' />
+        A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). Flowers produce gametophytes, which in flowering plants consist of a few haploid cells which produce gametes. The "male" gametophyte, which produces non-motile sperm, is enclosed within pollen grains; the "female" gametophyte is contained within the ovule. When pollen from the anther of a flower is deposited on the stigma, this is called pollination. Some flowers may self-pollinate, producing seed using pollen from the same flower or a different flower of the same plant, but others have mechanisms to prevent self-pollination and rely on cross-pollination, when pollen is transferred from the anther of one flower to the stigma of another flower on a different individual of the same species
+        </div>
     </div>
   );
 }
